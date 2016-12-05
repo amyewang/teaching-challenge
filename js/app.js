0d268866048b2d4205c04f185ab35dd70db5f8c7
Amy Wang <amyewang115@gmail.com>
2016-12-04 22:10:35 -0800

    added starter code

---
+++ js/app.js
@@ -0,0 +1,53 @@
+/**
+* Given this array of different fruits and vegetables called "food"
+*/
+var food = [{name: 'tomato', type: 'fruit'},
+            {name: 'apple', type:'fruit'},
+            {name: 'banana', type:'fruit'},
+            {name: 'orange', type:'fruit'},
+            {name: 'peach', type:'fruit'},
+            {name: 'cucumber', type:'vegetable'},
+            {name: 'lettuce', type:'vegetable'},
+            {name: 'carrot', type:'vegetable'},
+            {name: 'beet', type:'vegetable'},
+            {name: 'onion', type:'vegetable'},
+          ];
+console.log(food);
+
+/**
+* 1. return an array of only the names of the foods
+* use pluck
+* remember to print your result to the console (hint: you can use the value function)!
+*/
+
+
+/**
+* 2. return an array of foods with the type fruit
+* use filter
+* remember to print your result to the console!
+*/
+
+
+/**
+* 3. return an array of foods that change the type of vegetables from "vegetable" to "gross"
+* use map
+* remember to print your result to the console!
+*/
+
+
+/**
+* 4. return an array of foods that change the type of all foods to "produce",
+* practice chaining and only take the first two elements
+* use map, take
+* remember to print your result to the console!
+*/
+
+
+
+
+
+/**
+* BONUS: use the generate function to generate an indefinite list of random numbers
+* only return unique numbers
+* print to the console using the each function
+*/
